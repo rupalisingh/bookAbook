071c1213cb221c06ad99c983ae7480f21dea9c50
Rupali Singh <rupalisingh2304@gmail.com>
2024-03-23 02:18:19 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container, AppBar, Toolbar, Typography } from '@mui/material';
 import LoginPage from './Components/LoginPage';
 import HomePage from './Components/HomePage';
 import RegisterPage from './Components/Register';
@@ -15,12 +15,14 @@ const App = () => {
             <Typography variant="h6">bookAbook</Typography>
           </Toolbar>
         </AppBar>
+          <Container maxWidth="lg" style={{ marginTop: '20px' }}>
           <Routes>
             <Route exact path="/" element={<HomePage />} />
             <Route exact path="/login" element={<LoginPage />} />
             <Route exact path="/register" element={<RegisterPage />} />
             <Route exact path = "/user-profile-setup" element = {<UserProfileSetup/>} />
           </Routes>
+          </Container>
       </div>
     </Router>
   );
